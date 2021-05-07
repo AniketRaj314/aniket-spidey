6fe77dcb0c520334289f6787e41942b30cb43a2d
Aniket Raj <aniketronaldo10@gmail.com>
2021-05-07 16:10:59 +0530

    Shifted Background image to local storage

--- src/data.js
+++
@@ -1,5 +0,0 @@
-const DATA = {
-    backgroundImagePath: "http://4kwallpapers.com/images/walls/thumbs_3t/167.jpg",
-};
-
-export default DATA;
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import DATA from "data";
 
 import Background from "components/background/background.component";
 
 class Main extends React.Component {
     render() {
-        return <Background imagePath={DATA.backgroundImagePath} />;
+        return <Background imagePath={"/background.jpg"} />;
     }
 }
 
