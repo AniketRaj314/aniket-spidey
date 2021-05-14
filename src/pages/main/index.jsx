6e7214cb6a38229ad9235f85daccc6f73a64a7c2
Aniket Raj <aniketronaldo10@gmail.com>
2021-05-14 11:00:20 +0530

    Added Scroll Progress Check on Top Left

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 import Background from "components/background/background.component";
 import Internships from "sections/Internships/internships.section";
+import ScrollProgress from "components/ScrollProgress/scrollprogress.component";
 import "./main.styles.css";
 
 class Main extends React.Component {
     render() {
         return (
-            <>
+            <div className="container">
                 <Background imagePath={"/images/background.jpg"} />
                 <Internships size={3} />
-            </>
+                <ScrollProgress />
+            </div>
         );
     }
 }
