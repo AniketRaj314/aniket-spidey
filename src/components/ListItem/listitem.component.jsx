fa6be30418f05d4add51655987cf4e4be916309c
Aniket Raj <aniketronaldo10@gmail.com>
2021-05-17 11:49:15 +0530

    Converted ListItem to Functional Component with React Hooks for Image Looping

--- src/components/ListItem/listitem.component.jsx
+++ src/components/ListItem/listitem.component.jsx
@@ -1,51 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./listitem.styles.css";
 
-class ListItem extends React.Component {
-    constructor(props) {
-        super(props);
+const imageVariants = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 1,
+        transition: {
+            duration: 1,
+        },
+    },
+};
 
-        this.state = {
-            logoIndex: 0,
-        };
+export default function ListItem(props) {
+    const { skill, logo, description } = props;
+    const [logoIndex, setLogoIndex] = useState(0);
+    const milliseconds = 2000;
 
-        if (Array.isArray(this.props.logo)) {
-            this.setTimer(3000);
-        }
-    }
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if (Array.isArray(logo)) {
+                setLogoIndex((logoIndex + 1) % logo.length);
+            }
+        }, milliseconds);
 
-    setTimer(interval) {
-        setInterval(() => {
-            const { logoIndex } = this.state;
-            this.setState({ logoIndex: (logoIndex + 1) % this.props.logo.length });
-        }, interval);
-    }
+        return () => clearInterval(timer);
+    });
 
-    render() {
-        const { logoIndex } = this.state;
-        const { skill, logo, description } = this.props;
-        return (
-            <div className="skill-item">
-                {Array.isArray(logo) ? (
-                    <motion.img
-                        src={logo[logoIndex]}
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ duration: 2 }}
-                        className="skill-logo"
-                        alt={skill}
-                    />
-                ) : (
-                    <img src={logo} className="skill-logo" alt={skill} />
-                )}
-                <div className="skill-description">
-                    <h2>{skill}</h2>
-                    <p>{description}</p>
-                </div>
+    return (
+        <div className="skill-item">
+            {Array.isArray(logo) ? (
+                <motion.img
+                    src={logo[logoIndex]}
+                    initial="hidden"
+                    animate="visible"
+                    variants={imageVariants}
+                    className="skill-logo"
+                    alt={skill}
+                />
+            ) : (
+                <img src={logo} className="skill-logo" alt={skill} />
+            )}
+            <div className="skill-description">
+                <h2>{skill}</h2>
+                <p>{description}</p>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default ListItem;
